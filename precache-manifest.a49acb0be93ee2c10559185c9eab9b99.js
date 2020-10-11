f1eb942ee584e19a479a1dea23694d5b528e5671
mmunoz <mmunoz@evicertia.com>
2020-10-11 22:12:06 +0200

    Updates

---
+++ precache-manifest.a49acb0be93ee2c10559185c9eab9b99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c6e58101c112df1061cb262e7384c2",
+    "url": "/testapp.github.io/index.html"
+  },
+  {
+    "revision": "8eb916794175e9b7fa92",
+    "url": "/testapp.github.io/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/testapp.github.io/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testapp.github.io/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb916794175e9b7fa92",
+    "url": "/testapp.github.io/static/js/main.0095d728.chunk.js"
+  },
+  {
+    "revision": "3cd5edfb5ded36162938",
+    "url": "/testapp.github.io/static/js/runtime-main.2c500846.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/testapp.github.io/static/media/logo.ee7cd8ed.svg"
+  }
+]);
